26705d4a5875224a3c0ea9a40040a42b1b8d0d7a
Sarah Teklemariam <sarah.f.tsion@gmail.com>
2022-02-28 02:18:17 -0500

    added question seven

--- js/app.js
+++ js/app.js
@@ -5,29 +5,32 @@ console.log('Hello');
 let username= prompt('what is your name?').toLowerCase();
 alert('Hello ' + username + ', Welcome to my page!');
 let score = 0;
-
+function sevenQuestion(){
+  favoriteChocolate();
+  ageQuestion();
+  favoriteMac();
+  temperatureScale();
+  favoriteNumber();
+  favoriteFruits();
+  favoriteFoods();
+}
+sevenQuestion();
 //Question 1:
 
 function favoriteChocolate(){
   let doesAnyoneLikeChoclate = prompt('Do you like Choclate?');
-
   if (doesAnyoneLikeChoclate.toLowerCase() === 'yes' || doesAnyoneLikeChoclate.toLowerCase() === 'y') {
-    //console.log('Me too.');
     alert('Me too');
     score++;
-
   }
   else if(doesAnyoneLikeChoclate.toLowerCase() === 'no'|| doesAnyoneLikeChoclate.toLowerCase() === 'n') {
-  //console.log('Yes, you do, you\'re in denial');
     alert('Yes, you do, you\'re in denial.');
-
   }
   else {
-    //console.log('You need to answer with \'yes\' or \'no\' ');
     alert('You need to answer with \'yes\' or \'no\' ');
   }
 }
-favoriteChocolate();
+
 
 // //Question 2:
 
@@ -35,38 +38,31 @@ function ageQuestion(){
   let age = prompt('How old are you?');
   console.log(typeof age);
   if (parseInt(age) >= 18) {
-  //console.log('You are good to go');
     alert('You are good to go.');
     score++;
-
   }
   else if(parseInt(age) < 18) {
-    //console.log('You are not old enough.');
     alert('You are not old enough.');
   }
   else {
-  //console.log('you need to answer with numbers only.');
     alert('You need to answer with numbers only.');
   }
 }
-ageQuestion();
+
 
 //Question 3
 
 function favoriteMac(){
   let user = prompt('Do you like Mac computer?');
   if (user.toLowerCase() === 'yes' || user.toLowerCase()==='y'){
-  // console.log('Me too.');
     alert('Me too');
     score++;
-
   }
   else if (user.toLowerCase() === 'no' || user.toLowerCase()==='n'){
-  // console.log('Yes, you do, you\'re in denial.');
     alert('Yes, you do, you\'re in denial.');
   }
 }
-favoriteMac();
+
 
 //Question 4
 
@@ -78,18 +74,12 @@ function temperatureScale(){
     score++;
   }
   else if (temprature > 45){
-  //console.log('it is a great weather outside, wear light weight cloth.')
     alert('The weather is great today, wear light weight cloth.');
   }
   else{
     alert('answer in numbers only');
   }
 }
-temperatureScale();
-
-
-
-
 
 
 //Question 5:
@@ -111,25 +101,17 @@ function favoriteNumber(){
     }
   }
 }
-favoriteNumber();
-
-
-
-
-
-
 
 //Question 6
 
 function favoriteFruits(){
   let attemptsRemaining = 6; 
-  let favFruit = ['kiwi', 'pineapple', 'orange', 'grape', 'watermelon'];
+  let favFruit = ['kiwi', 'pineapple', 'orange', 'grapes', 'watermelon'];
 
 
   for (let i = 0; i < 6; i++) {
     let userAnsw = prompt('Can you guess my favorite fruit?');
     let guess = userAnsw.toLowerCase();
-
     for (let j = 0; j < favFruit.length; j++){
       if (guess === favFruit[j]) {
         alert('You are correct!');
@@ -139,83 +121,36 @@ function favoriteFruits(){
       }
     }
     if (i !== 6){
-    // console.log(`you have ${i - 1} attempts remaining.`);
       alert(`you have ${attemptsRemaining - i - 1} attempts remaining.`);
-      //
-      //attemptsRemaining--;
       score++;
-
     }
   }
 }
-favoriteFruits();
 
+//Question 7.
+
+function favoriteFoods(){
+
+  let favFoods = ['lasagna','injera','parm chicken','pizza','spagetti'];
+  let attemptsRemaining7 = 6; 
+  let correctAnswer7 = 0;
+  while (attemptsRemaining7) {
+    for (let j = 0; j < favFoods.length; j++){
+      let userAnswer7 = prompt('can you guess my favourite foods?');
+      if (userAnswer7 === favFoods[0] || userAnswer7 === favFoods[1] || userAnswer7 === favFoods[2] || userAnswer7 === favFoods[3] || userAnswer7 === favFoods[4]);
+      alert('That is correct.');
+      correctAnswer7++;
+      j = 6;
+      break;
+    } if (userAnswer7 !== favFoods[j]){
+      alert(`That is not correct.You have ${attemptsRemaining7 - 1} attempts remaining!`);
+    }
+    attemptsRemaining7--;
+    if (attemptsRemaining7 === 0) {
+      alert('opps you run out of attempts, try again later');
+      break;
+    }
+  }
+}
 alert('Great job'  + ' ' + username);
 alert('Nice Job, you got ' + score + ' out of 7');
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//   correctAnswer();
-
-// // alert('My favorite fruits are: apple, strawberry, orange, grape and watermelon');
-// let favFruit = ['orange','pineapple','grapes','watermelon','banana','strawberry','kiwi'];
-
-// let attempsremaining = 7;
-//  for (let i = 0; i < favFruit.length; i++); {
-//    let fruit = prompt('can you guess my favourite fruits?');{
-//       if(fruit === favFruit[i]){
-//        //console.log('You are correct.');
-//        alert('You are correct.');
-//        score++;
-//        break;
-//      }
-//      else if (fruit !== favFruits){
-//        //console.log('please try again.');
-//        alert(`you have ${attemptsRemaining - 1} attempts remaining,try again`);
-//        attemptsRemaining--;
-//      }
-//    }
-//  }
- 
-
-// alert('your final score is '+ score + 'out of 7. please try again later.');
